c321db435386b42d53484e760858587a4d762544
RENNES\l.masson <l.masson@rns.ecolescreatives.com>
2023-01-31 16:28:39 +0100

    script load scene

---
+++ Assets/Scripts/Tools/S_LoadScene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class S_LoadScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SceneManager.LoadScene("Scene_Pierre_Deco", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Scene_Shader2", LoadSceneMode.Additive);
+        SceneManager.LoadScene("ThibaultDecoverticalSlice", LoadSceneMode.Additive);
+    }
+}
