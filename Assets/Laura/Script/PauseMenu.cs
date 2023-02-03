7aa628fc3629e855b698b52f6716eead6812b47c
Laura <l.schavsinski@rns.ecolescreatives.com>
2023-02-03 09:21:26 +0100

    Menu Laura

---
+++ Assets/Laura/Script/PauseMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject PauseMenuMain;
+    [SerializeField] private bool isPaused;
+
+   private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            ActivateMenu();
+        }
+
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        PauseMenuMain.SetActive(true);
+    }
+
+    public void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        PauseMenuMain.SetActive(false);
+        isPaused = false;
+    }
+}
