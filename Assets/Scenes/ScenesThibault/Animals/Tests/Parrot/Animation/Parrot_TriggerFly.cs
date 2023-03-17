57ae22a08f8b974d8d1b393d7395f6d0fd40190e
RENNES\t.jovelin <t.jovelin@rns.ecolescreatives.com>
2023-03-17 15:59:23 +0100

    Création de la vallée des foreurs

--- Assets/Scenes/ScenesThibault/Animals/Tests/Parrot/Animation/Parrot_TriggerFly.cs
+++ Assets/Scenes/ScenesThibault/Animals/Tests/Parrot/Animation/Parrot_TriggerFly.cs
@@ -5,17 +5,14 @@ using UnityEngine;
 public class Parrot_TriggerFly : MonoBehaviour
 {
     public bool BoolParrotFly;
-    public int ParrotFlyRandom;
     public GameObject Parot_TriggerZoneFly;
     public Animator animator;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
+            animator.SetInteger("ParrotFlyRandom", Random.Range(0, 100));
             animator.SetBool("BoolParrotFly", true);
-
-        animator.SetInteger("ParrotFlyRandom", Random.Range(0, 3));
-        animator.SetTrigger("TriggerParrotFly");
     }
 
     private void OnTriggerExit(Collider other)
