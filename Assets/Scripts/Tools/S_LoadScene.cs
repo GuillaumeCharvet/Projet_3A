4c14c9055b4c34720eae07d155561a336f4a05ab
GuillaumeCharvet <guicharvet@orange.fr>
2023-03-30 09:19:57 +0200

    Added slope run speed modulation + ...

--- Assets/Scripts/Tools/S_LoadScene.cs
+++ Assets/Scripts/Tools/S_LoadScene.cs
@@ -8,12 +8,13 @@ public class S_LoadScene : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        
-        SceneManager.LoadScene("Scene_William2", LoadSceneMode.Additive);
-        SceneManager.LoadScene("Scene_Pierre_Deco", LoadSceneMode.Additive);
-        SceneManager.LoadScene("Scene_Shader2", LoadSceneMode.Additive);
-        SceneManager.LoadScene("ThibaultDecoverticalSlice", LoadSceneMode.Additive);
-        SceneManager.LoadScene("Scene_Ocean", LoadSceneMode.Additive);
-        
+        if (ManagerManager.Instance.GetComponent<S_Debugger>().buildReady)
+        {
+            SceneManager.LoadScene("Scene_William2", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Scene_Pierre_Deco", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Scene_Shader2", LoadSceneMode.Additive);
+            SceneManager.LoadScene("ThibaultDecoverticalSlice", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Scene_Ocean", LoadSceneMode.Additive);
+        }
     }
 }
