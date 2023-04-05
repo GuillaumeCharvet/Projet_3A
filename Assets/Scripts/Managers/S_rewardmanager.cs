6568b4ddea0562d01145c94b4cd5c6d0d1c92f9d
GueguinWilliam <74670003+GueguinWilliamGDB1B@users.noreply.github.com>
2023-04-05 18:57:42 +0200

    Script --  collecte d'objets et son ui

---
+++ Assets/Scripts/Managers/S_rewardmanager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class S_rewardmanager : MonoBehaviour
+{
+    [SerializeField]
+    // REWARDS TECHNOS //
+    public GameObject Ile1_stele_1;
+    public GameObject Ile1_stele_2;
+    public GameObject Ile1_stele_3;
+    public GameObject Ile1_stele_4;
+
+    public GameObject Ile2_stele_1;
+    public GameObject Ile2_stele_2;
+    public GameObject Ile2_stele_3;
+    public GameObject Ile2_stele_4;
+
+    // REWARDS NATURE //
+
+    public GameObject Ile1_nature_1;
+    public GameObject Ile1_nature_2;
+    public GameObject Ile1_nature_3;
+    public GameObject Ile1_nature_4;
+
+    public GameObject Ile2_nature_1;
+    public GameObject Ile2_nature_2;
+    public GameObject Ile2_nature_3;
+    public GameObject Ile2_nature_4;
+
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
