49d02cd432422c3bf112591e4ad88643b6b660b4
Laura <l.schavsinski@rns.ecolescreatives.com>
2023-05-24 15:26:00 +0200

    Test de rapatriement Louis

--- Assets/Laura/Script/Options/Icon/S_IconOptions.cs
+++ Assets/Laura/Script/Options/Icon/S_IconOptions.cs
@@ -6,26 +6,38 @@ public class S_IconOptions : MonoBehaviour
 {
     [SerializeField] private GameObject Display;
     [SerializeField] private GameObject Sounds;
-    [SerializeField] private GameObject Controls;
+    [SerializeField] private GameObject ControlsController;
+    [SerializeField] private GameObject ControlsKeyboard;
 
     public void ActivateMenuDisplay()
     {
         Display.SetActive(true);
         Sounds.SetActive(false);
-        Controls.SetActive(false);
+        ControlsController.SetActive(false);
+        ControlsKeyboard.SetActive(false);
     }
 
     public void ActivateMenuSounds()
     {
         Display.SetActive(false);
         Sounds.SetActive(true);
-        Controls.SetActive(false);
+        ControlsController.SetActive(false);
+        ControlsKeyboard.SetActive(false);
     }
 
-    public void ActivateMenuControls()
+    public void ActivateMenuControlsController()
     {
         Display.SetActive(false);
         Sounds.SetActive(false);
-        Controls.SetActive(true);
+        ControlsController.SetActive(true);
+        ControlsKeyboard.SetActive(false);
+    }
+
+    public void ActivateMenuControlsKeyboard()
+    {
+        Display.SetActive(false);
+        Sounds.SetActive(false);
+        ControlsController.SetActive(false);
+        ControlsKeyboard.SetActive(true);
     }
 }
