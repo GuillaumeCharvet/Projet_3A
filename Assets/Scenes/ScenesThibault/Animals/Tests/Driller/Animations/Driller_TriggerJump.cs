16c80bc2a7e77d256ace00713ed6a8e341824dd1
GuillaumeCharvet <guicharvet@orange.fr>
2023-05-25 17:56:53 +0200

    driller step up

--- Assets/Scenes/ScenesThibault/Animals/Tests/Driller/Animations/Driller_TriggerJump.cs
+++ Assets/Scenes/ScenesThibault/Animals/Tests/Driller/Animations/Driller_TriggerJump.cs
@@ -8,29 +8,49 @@ public class Driller_TriggerJump : MonoBehaviour
     public GameObject Driller_TriggerZoneJump;
     public Animator animator;
 
-    private Vector3 targetRotation;
+    private float speedRotation = 190f;
+    private Quaternion targetRotation;
+    private Vector3 targetRotationEuler = Vector3.zero, targetRotationEuler2 = Vector3.zero;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            targetRotation = Vector3.zero;
+            var playerDirection = other.transform.position - transform.parent.position;
+            var fleeingDirection = new Vector3(playerDirection.x, 0f, playerDirection.z);
+            targetRotationEuler = fleeingDirection;
+            targetRotation = Quaternion.LookRotation(fleeingDirection, transform.parent.up);
+            targetRotationEuler2 = targetRotation * Vector3.forward;
+
+            StartCoroutine(StartRotating());
         }
-        animator.SetBool("BoolDrillerJump", true);
     }
+
+    /*
+    private void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.parent.position, transform.parent.position + targetRotationEuler);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.parent.position + Vector3.up * 0.1f, transform.parent.position + Vector3.up * 0.1f + targetRotationEuler2);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.parent.position + Vector3.up * 0.2f, transform.parent.position + Vector3.up * 0.2f + transform.parent.forward);
+    }
+    */
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
-            animator.SetBool("BoolDrillerJump", false);
+        if (other.tag == "Player") animator.SetBool("BoolDrillerJump", false);
     }
 
     private IEnumerator StartRotating()
     {
-        var t = 0f;
-        while (true)
+        while (Vector3.Angle(-transform.parent.forward, -(targetRotation * Vector3.forward)) > 5f)
         {
+            transform.parent.rotation = Quaternion.RotateTowards(transform.parent.rotation, targetRotation, Time.deltaTime * speedRotation);
             yield return new WaitForEndOfFrame();
         }
+        animator.SetBool("BoolDrillerJump", true);
     }
 }
