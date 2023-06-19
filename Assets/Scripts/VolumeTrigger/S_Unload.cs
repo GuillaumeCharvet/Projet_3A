298a3436f2429cbe3dd1d0bfa2843c5ae44b5bf2
EloGko[Eloi MICHEL] <eloi.michel.bros@orange.fr>
2023-06-19 18:22:22 +0200

    unload zone

--- Assets/Scripts/VolumeTrigger/S_Unload.cs
+++ Assets/Scripts/VolumeTrigger/S_Unload.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class S_Unload : MonoBehaviour
 {
-    public GameObject package;
+    public GameObject package1;
+    public GameObject package2;
+    public GameObject package3;
+    public GameObject package4;
+    public GameObject package5;
 
     // Start is called before the first frame update
     public void OnTriggerEnter(Collider other)
     {
-        package.SetActive(false);
+        package1.SetActive(false);
+        package2.SetActive(false);
+        package3.SetActive(false);
+        package4.SetActive(false);
+        package5.SetActive(false);
     }
 
     public void OnTriggerExit(Collider other)
     {
-        package.SetActive(true);
+        package1.SetActive(true);
+        package2.SetActive(true);
+        package3.SetActive(true);
+        package4.SetActive(true);
+        package5.SetActive(true);
     }
     // Update is called once per frame
     void Update()
